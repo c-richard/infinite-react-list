ce70ffb88de8ebf909f20a5bfa700ac99a8745a7
c-richard <richardcarruthers0@gmail.com>
2020-08-10 08:09:59 +0100

    fix/clean npm deployment

--- src/InfiniteList.js
+++ src/InfiniteList.js
@@ -1,14 +1,13 @@
 import React, { Component} from "react";
-import "./App.css";
 
-class App extends Component{
+class List extends Component{
   render(){
     return(
-      <div className="App">
+      <div className="List">
         <h1> Hello, World! </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default List;
