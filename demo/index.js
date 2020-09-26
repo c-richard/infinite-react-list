ce5fd949dff8057808bf8a3fba47bb8702c92879
c-richard <richardcarruthers0@gmail.com>
2020-09-26 20:29:52 +0100

    use iterator protocol next() over itemGenerator w/ example generator generated list

--- demo/index.js
+++ demo/index.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import InfiniteList from '../src/InfiniteList';
+import 'regenerator-runtime';
 
 const App = () => {
   let f0 = 0;
   let f1 = 1;
+  let old = 0;
 
-  const itemGenerator = () => {
-    const oldF0 = f1;
-    f1 += f0;
-    f0 = oldF0;
+  function* fibonacho(maxN) {
+    for (f0 = 0, f1 = 1, old = 0; f1 < maxN; old = f1, f1 += f0, f0 = old) {
+      yield (
+        <div key={`${f0}${f1}`}>
+          {f0}
+        </div>
+      );
+    }
+  }
 
-    return (
-      <div key={`${f0}${f1}`}>
-        <b>{f0}</b>
-      </div>
-    );
-  };
+  const itemGenerator = fibonacho(10000000);
 
   return (
     <div>
       <h1>demonstration</h1>
-      <InfiniteList itemGenerator={itemGenerator} />
+      <InfiniteList next={() => itemGenerator.next()} />
     </div>
   );
 };
--- src/InfiniteList.js
+++ src/InfiniteList.js
@@ -1,21 +1,26 @@
 import React, { useState, useRef, useEffect } from 'react';
 import useIntersectionObserver from './useIntersectionObserver';
 
-const ItemGenerator = ({ itemGenerator }) => {
+const ItemGenerator = ({ next }) => {
   const [items, setItems] = useState([]);
   const [isExtendingList, setIsExtendingList] = useState(false);
   const [waypointAboveScroll, setWaypointAboveScroll] = useState(false);
+  const [complete, setComplete] = useState(false);
 
   const observerRef = useRef(null);
   const observeeRef = useRef(null);
   const [isIntersecting] = useIntersectionObserver(observerRef, observeeRef);
 
   useEffect(() => {
-    if ((isIntersecting || waypointAboveScroll) && !isExtendingList) {
+    if (!complete && (isIntersecting || waypointAboveScroll) && !isExtendingList) {
       setIsExtendingList(true);
       setTimeout(() => {
-        const newItem = itemGenerator();
-        setItems([...items, newItem]);
+        const {
+          value,
+          done,
+        } = next();
+        setItems([...items, value]);
+        setComplete(done);
         setIsExtendingList(false);
         setWaypointAboveScroll(observeeRef.current.offsetTop < observerRef.current.scrollTop);
       }, 100);
@@ -23,7 +28,7 @@ const ItemGenerator = ({ itemGenerator }) => {
   }, [isIntersecting, isExtendingList, waypointAboveScroll]);
 
   return (
-    <ul ref={observerRef} style={{ border: '1px solid salmon', maxHeight: '150px', overflow: 'scroll' }}>
+    <ul ref={observerRef} style={{ border: '1px solid salmon', maxHeight: '150px', overflow: 'scroll', position: 'relative' }}>
       { items.slice(0, -20).map((a) => a) }
       <div ref={observeeRef} style={{ border: '1px solid green' }} />
       { items.slice(-20).map((a) => a) }
