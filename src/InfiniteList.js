fcd1db91cf900e8da4c81b8ebe960c88aa639a21
c-richard <richardcarruthers0@gmail.com>
2020-09-26 19:46:35 +0100

    generate list before scrolling to end

--- src/InfiniteList.js
+++ src/InfiniteList.js
@@ -4,26 +4,29 @@ import useIntersectionObserver from './useIntersectionObserver';
 const ItemGenerator = ({ itemGenerator }) => {
   const [items, setItems] = useState([]);
   const [isExtendingList, setIsExtendingList] = useState(false);
+  const [waypointAboveScroll, setWaypointAboveScroll] = useState(false);
 
   const observerRef = useRef(null);
   const observeeRef = useRef(null);
   const [isIntersecting] = useIntersectionObserver(observerRef, observeeRef);
 
   useEffect(() => {
-    if (isIntersecting && !isExtendingList) {
+    if ((isIntersecting || waypointAboveScroll) && !isExtendingList) {
       setIsExtendingList(true);
       setTimeout(() => {
         const newItem = itemGenerator();
         setItems([...items, newItem]);
         setIsExtendingList(false);
+        setWaypointAboveScroll(observeeRef.current.offsetTop < observerRef.current.scrollTop);
       }, 100);
     }
-  }, [isIntersecting, isExtendingList]);
+  }, [isIntersecting, isExtendingList, waypointAboveScroll]);
 
   return (
     <ul ref={observerRef} style={{ border: '1px solid salmon', maxHeight: '150px', overflow: 'scroll' }}>
-      { items.map((a) => a) }
-      <div ref={observeeRef} />
+      { items.slice(0, -20).map((a) => a) }
+      <div ref={observeeRef} style={{ border: '1px solid green' }} />
+      { items.slice(-20).map((a) => a) }
     </ul>
   );
 };
